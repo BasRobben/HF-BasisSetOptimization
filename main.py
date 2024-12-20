31c72bd98f49183daf62f7aa704182d426369d4c
BasRobben <basjhuu@gmail.com>
2024-12-20 10:46:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 def main():
     print('Hello world :)')
 
+    x = np.linspace(0,1,1000)
+    y = np.sin(2*np.pi*x)
+    plt.plot(x,y)
+    plt.show()
+
     return 
 
 if __name__ == '__main__':
