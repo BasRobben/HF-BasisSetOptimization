626919a8dcaedd754f2c67adc0c7a327df72e6a5
henkg <henkgongriep.hg@gmail.com>
2025-01-10 10:47:29 +0100

    Scratch code, prints out individual primitives, for now only the 1S of hydrogen. Also shows a plot of the primitives, the summed primitives and STO 1S orbitals.

--- Scratch code.py
+++ Scratch code.py
@@ -1,14 +1,51 @@
-def varied_basis_set(num_gto):
-    from pyqint import cgf
+import numpy as np
+from math import factorial
+import matplotlib.pyplot as plt
+from scipy import optimize
 
-    basis_set = cgf([0.0, 0.0, 0.0])
 
-    c = 1/num_gto
-    alpha = 1
+def optimise_primitive_gaussians(num_primitives, zeta, l, m, n, r):
 
-    for i in range(num_gto):
-        cgf.add_gto(basis_set, c, alpha, 0, 0, 0)
+    # calculate slater type orbital of 1S H2
+    sto_1s = np.sqrt((zeta ** 3) / np.pi) * np.exp(-zeta * r)
 
-    return basis_set
+    # initial guess matrix (1S, STO-3G, Hydrogen) [1st row: alphas, 2nd row: coefficients]
+    coeff = np.array([[0.3425250914E+01, 0.6239137298E+00, 0.1688554040E+00],
+                      [0.1543289673E+00, 0.5353281423E+00, 0.4446345422E+00]])
 
-print(f'varied basis set: {varied_basis_set(3)}')
+    # calculate normalisation, returns matrix [1 x 3] of normalisation constants
+    normalisation = (((2 * coeff[0, :] / np.pi) ** (3/4)) *
+                     np.sqrt((((8 * coeff[0, :]) ** (l + m + n)) * factorial(l) * factorial(m) * factorial(n)) /
+                             (factorial(2 * l) * factorial(2 * m) * factorial(2 * n))))
+
+    # calculate primitive gaussian per alpha, normalisation and coefficient
+    # store in matrix of size [n_primitives x i]
+    primitive_gaussian_array = np.zeros([num_primitives, len(r)])
+
+    for i in range(len(coeff[0, :])):
+        primitive_gaussian = normalisation[i] * coeff[1, i] * np.exp(-coeff[0, i] * (r ** 2))
+        primitive_gaussian_array[i] = primitive_gaussian
+
+    # optimize coeff matrix
+    # optimized_coeff = optimize.minimize(sto_1s, coeff, args=(r,), method='BFGS')
+    optimized_coeff = optimize.minimize(sto_1s, coeff, method='BFGS')
+
+    # construct the summed gaussian
+    sto3g = np.sum(primitive_gaussian_array, axis=0)
+
+    # make plot of primitive gaussians
+    plt.ylim(0, 1)
+    plt.xlim(0, 6)
+    plt.plot(r, primitive_gaussian_array[0])
+    plt.plot(r, primitive_gaussian_array[1])
+    plt.plot(r, primitive_gaussian_array[2])
+    plt.plot(r, sto3g)
+    plt.plot(r, sto_1s)
+    plt.show()
+
+    return primitive_gaussian_array, sto3g, optimized_coeff
+
+
+
+
+print(optimise_primitive_gaussians(3, 1.24, 0, 0, 0, np.linspace(0, 6, 1000)))
